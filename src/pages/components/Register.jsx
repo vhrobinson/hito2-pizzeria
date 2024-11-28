84751447875c7bd310051a4d6d3669bf96ad6e83
vhrobinson <vhrobinson@gmail.com>
2024-11-27 21:31:50 -0300

    pude cambiar los colores detailwin para que se vieran estaban muy oscuros

--- src/pages/components/Register.jsx
+++ src/pages/components/Register.jsx
@@ -16,30 +16,36 @@ function Register() {
     return (
   
         <form  className="max-w-sm mx-auto">
-          <h1 className="text-3xl">Login </h1>
+          <h1 className="text-3xl">Register </h1>
             <div className="mb-5">
-                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Email</label>
-                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black
-                 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@email.com" required />
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-600 text-white">Email</label>
+                <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-white-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :bg-gray-700 border-gray-600 placeholder-gray-400 
+
+                 :focus:ring-blue-500 :focus:border-blue-500 :shadow-sm-light" placeholder="name@email.com" required />
             </div>
             <div className="mb-5">
-                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black
+                <label for="password" className="block mb-2 text-sm font-medium text-gray-600 :text-white
+
                 ">Password</label>
           <input type="password" id="password" value={password}
-            onChange={(e) => setPassword(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black
-                 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            onChange={(e) => setPassword(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-white-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :bg-gray-700 :border-gray-600 :placeholder-gray-400 
+
+                 :focus:ring-blue-500 :focus:border-blue-500 :shadow-sm-light" required />
             </div>
             <div className="mb-5">
-                <label for="rpassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black
+                <label for="rpassword" className="block mb-2 text-sm font-medium text-white-600 :text-white
+
                 ">Repeat Password</label>
           <input type="password" id="rpassword" value={rPassword}
-            onChange={(e) => setrPassword(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black
-                 dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+            onChange={(e) => setrPassword(e.target.value)} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-600 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 :bg-gray-700 :border-gray-600 :placeholder-gray-400 :text-white
+
+                 :focus:ring-blue-500 :focus:border-blue-500 :shadow-sm-light" required />
             </div>
             
          
-            <button onClick={validarPassword} type="submit" className="text-black
-             bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+            <button onClick={validarPassword} type="submit" className="text-white
+
+             bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center :bg-blue-600 :hover:bg-blue-700 :focus:ring-blue-800">Login</button>
             
         </form>
   
